2db58f5c081bfadd200d346753b2900492155845
Ketan-Rahane <160168514+Ketan-Rahane@users.noreply.github.com>
2025-06-09 11:12:10 -0700

    Add files via upload

---
+++ List.py
@@ -0,0 +1,10 @@
+my_list=[10,20,30,40,50]
+print(my_list)
+
+my_list.pop()
+print("list after poped():",my_list)
+
+my_list.remove(20)
+print("list after remove(30):",my_list)
+
+print(my_list[25]) 

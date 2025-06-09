1143baf3dd2474750f6552805cd8a8ae8e6b109e
Ketan-Rahane <160168514+Ketan-Rahane@users.noreply.github.com>
2025-06-09 11:19:09 -0700

    List.py

--- List.py
+++ List.py
@@ -1,5 +1,5 @@
 my_list=[10,20,30,40,50]
-print(my_list)
+print("my_list:",my_list)
 
 my_list.pop()
 print("list after poped():",my_list)
@@ -8,3 +8,14 @@ my_list.remove(20)
 print("list after remove(30):",my_list)
 
 print(my_list[25]) 
+
+
+#Output:
+my_list: [10, 20, 30, 40, 50]
+list after poped(): [10, 20, 30, 40]
+list after remove(20): [10, 30, 40]
+Traceback (most recent call last):
+  File "d:\Python\List.py", line 10, in <module>
+    print(my_list[25])
+          ~~~~~~~^^^^
+IndexError: list index out of range.

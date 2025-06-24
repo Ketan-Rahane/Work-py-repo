2c5b11f7d3b708a8ca25839c6c66b9fd1e8fb2f3
Ketan-Rahane <160168514+Ketan-Rahane@users.noreply.github.com>
2025-06-24 13:20:23 +0530

    Add files via upload

---
+++ Class.py
@@ -0,0 +1,17 @@
+
+
+class Employee:
+    name="Ram"
+    salary=40000
+
+class Phone:
+    ph_no=1234567896
+    email="ram12@gmail.com"
+
+obj1=Employee()
+print("Name of Employee:",obj1.name)
+print("Salary of Employee:",obj1.salary)
+
+obj2=Phone()
+print("Phone number of employee is:",obj2.ph_no)
+print("Email of employee is :",obj2.email)

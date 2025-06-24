c27312c4fa5eb448c0c4b18acf72e67826404670
Ketan-Rahane <160168514+Ketan-Rahane@users.noreply.github.com>
2025-06-24 13:22:33 +0530

    Class.py

--- Class.py
+++ Class.py
@@ -15,3 +15,12 @@ print("Salary of Employee:",obj1.salary)
 obj2=Phone()
 print("Phone number of employee is:",obj2.ph_no)
 print("Email of employee is :",obj2.email)
+
+
+#Outputs:
+
+Name of Employee: Ram
+Salary of Employee: 40000
+Phone number of employee: 1234567896
+Email of employee: ram12@gmail.com
+PS D:\Python\Homework> 

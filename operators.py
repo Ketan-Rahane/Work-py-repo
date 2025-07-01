ac9392c0b2642c319b5077afb53ceee53d252f5b
Ketan-Rahane <160168514+Ketan-Rahane@users.noreply.github.com>
2025-07-02 00:52:24 +0530

    Operators.py

--- operators.py
+++ operators.py
@@ -1,71 +1,77 @@
 class Book:
-    def __init__(self,name,price):
-        self.name=name
+    def __init__(self,price):
+        
         self.price=price
 
     def __str__(self):
-        return f"{self.name}: {self.price}"
+        return  str(self.price)
     
     def __add__(self,other):
-        return Book(f"{self.name} + {other.name}",self.price + other.price)
-
-    def __sub__(self,other):
-        return Book(f"{self.name} - {other.name}",self.price - other.price)
+        return Book(self.price + other.price)
     
-    def __mul__(self, other):
-        return Book(f"{self.name} * {other.name}", self.price * other.price)
+    def __sub__(self,other):
+        return Book(self.price - other.price)
+    
+    def __mul__(self,other):
+        return Book(self.price * other.price)
     
     def __truediv__(self,other):
-        return Book(f"{self.name} / {other.name}",self.price / other.price)
+        return Book(self.price / other.price)
     
     def __floordiv__(self,other):
-        return Book(f"{self.name} // {other.name}",self.price // other.price)
+        return Book(self.price // other.price)
     
     def __mod__(self,other):
-        return Book(f"{self.name} %  {other.name}",self.price % other.price)
+        return Book(self.price % other.price)
     
     #def __pow__(self,other):
-       # return Book(f"{self.name} ** {other.name}",self.price ** other.price)
+     #   return Book(self.price ** other.price)
     
-    def __et__(self,other):
-        return Book(f"{self.name} == {other.name}",self.price == other.price)
+    def __eq__(self,other):
+        return Book(self.price == other.price)
     
     def __ne__(self,other):
-        return Book(f"{self.name} != {other.name}",self.price != other.price)
+        return Book(self.price != other.price)
     
     def __lt__(self,other):
-        return Book(f"{self.name} < {other.name}",self.price < other.price)
+        return Book(self.price < other.price)
     
     def __gt__(self,other):
-        return Book(f"{self.name} > {other.name}",self.price > other.price)
+        return Book(self.price > other.price)
     
-    def __le__(self, other):
-        return Book(f"{self.name} <= {other.name}", self.price <= other.price)
+    def __le__(self,other):
+        return Book(self.price <= other.price)
+    
+    def __ge__(self,other):
+        return Book(self.price >= other.price)
  
-    def __ge__(self, other):
-        return Book(f"{self.name} >= {other.name}", self.price >= other.price)
- 
-   
- 
- 
- 
- 
- 
- 
-B1=Book("Core Python",200)
-B2=Book("Advance Python",400)   
+B1=Book(200)
+B2=Book(400)   
 
+print("Price:",B1 + B2)
+print("Price:",B1 - B2)
+print("Price:",B1 * B2)
+print("Price:",B1 / B2)       
+print("Price:",B1 //B2)      
+print("Price:",B1 % B2)       
+#print("Price:",B1 **B2) 
+print("Price:",B1 ==B2)
+print("Price:",B1 !=B2)
+print("Price:",B1 < B2)
+print("Price:",B1 > B2)
+print("Price:",B1 <=B2)
+print("Price:",B1>= B2)
 
-print( B1 + B2)
-print(B1 - B2)
-print(B1 * B2)
-print(B1 / B2)       
-print(B1 // B2)      
-print(B1 % B2)       
-#print(B1 ** B2) 
-print(B1 == B2)
-print(B1 != B2)
-print(B1 < B2)
-print(B1 > B2)
-print(B1 <= B2)
-print(B1>=B2)
+#Output:
+Price: 600
+Price: -200
+Price: 80000
+Price: 0.5
+Price: 0
+Price: 200
+Price: False
+Price: True
+Price: True
+Price: False
+Price: True
+Price: False
